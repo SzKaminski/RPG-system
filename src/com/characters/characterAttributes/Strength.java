aea00a5e9c138114994c5abaaa767fb3eb203501
Szymon Kamiński <kamin.szymon@gmail.com>
2020-09-03 22:33:20 +0200

    typo fix/printer color added

---
+++ src/com/characters/characterAttributes/Strength.java
@@ -0,0 +1,12 @@
+package com.characters.characterAttributes;
+
+public class Strength extends CharAttribute {
+
+    public Strength(int value) {
+        super("Strength", "Str", value);
+    }
+
+    public Strength() {
+        super();
+    }
+}
